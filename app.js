00a62579156f28d9010090baa6059a4698782989
makiyo-ronkko <makiyo.ronkko@gmail.com>
2024-03-26 10:31:16 +0200

    master: feat: add new courses path

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ import { Hono } from 'https://deno.land/x/hono@v3.7.4/mod.ts';
 import { Eta } from 'https://deno.land/x/eta@v3.1.0/src/index.ts';
 // import * as countController from './countController.js';
 import * as feedbackController from './feedbackController.js';
+import * as courseController from './courseController.js';
 
-// const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
 
 app.get('/', feedbackController.showFeedbackForm);
 app.get('/feedbacks/:value', feedbackController.getFeedback);
 app.post('/feedbacks/:value', feedbackController.submitFeedback);
 app.post('/reset-feedbacks', feedbackController.resetFeedbacks);
+
+app.get('/courses', courseController.showForm);
+app.get('/courses/:id', courseController.showCourse);
+app.post('/courses', courseController.createCourse);
+app.post('/courses/:id', courseController.updateCourse);
+app.post('/courses/:id/delete', courseController.deleteCourse);
 
 // app.get('/', (c) => c.html(eta.render('index.eta', { title: 'Feedbacks' })));
 // app.get('/about', (c) => c.html(eta.render('index.eta', { title: 'About' })));
