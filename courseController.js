abc1d78dc2aa712cf20fd35b1c1f57c95477993f
makiyo-ronkko <makiyo.ronkko@gmail.com>
2024-03-27 11:15:42 +0200

    master: feat: add error handling for course name

--- courseController.js
+++ courseController.js
@@ -1,7 +1,18 @@
 import { Eta } from 'https://deno.land/x/eta@v3.1.0/src/index.ts';
+import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts';
 import * as courseService from './courseService.js';
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
+const validator = z.object({
+  name: z
+    .string({
+      message: 'The course name should be a string of at least 4 characters.',
+    })
+    .min(4, {
+      message: 'The course name should be a string of at least 4 characters.',
+    }),
+});
 
 const showForm = async (c) => {
   return c.html(
@@ -21,6 +32,16 @@ const showCourse = async (c) => {
 
 const createCourse = async (c) => {
   const body = await c.req.parseBody();
+  const validationResult = validator.safeParse(body);
+  if (!validationResult.success) {
+    return c.html(
+      eta.render('courses.eta', {
+        ...body,
+        courses: await courseService.listCourses(),
+        errors: validationResult.error.format(),
+      })
+    );
+  }
   await courseService.createCourse(body);
   return c.redirect('/courses');
 };
